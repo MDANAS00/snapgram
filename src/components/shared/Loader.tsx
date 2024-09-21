669377fa270af2fe277f04606f0f008c312f5dbb
mdanas00 <mohd44anas@gmail.com>
2024-09-21 19:40:27 +0530

    Project setup | Create signup page

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+const Loader = () => {
+  return (
+    <div className="fkex-center w-full">
+      <img
+        src="/public/assets/icons/loader.svg"
+        alt="loader"
+        width={24}
+        height={24}
+      />
+    </div>
+  );
+};
+
+export default Loader;
